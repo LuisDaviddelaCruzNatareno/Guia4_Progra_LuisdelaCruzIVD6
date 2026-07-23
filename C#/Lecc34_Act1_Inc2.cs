8e82eb92ef83a4333221a441501ccd4dd52ff7b9
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-07-22 21:06:44 -0600

    Add files via upload

---
+++ C#/Lecc34_Act1_Inc2.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Luis de la Cruz\nGrado y sección: IVD\nClave: 6\n--- CÁLCULO DE IMC ---\n");
+
+        Console.Write("Ingrese su peso en kg: ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese su altura en metros: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+        Console.WriteLine($"\nSu IMC es: {imc:F2}");
+
+        if (imc < 18.5)
+            Console.WriteLine("Estado: Bajo peso");
+        else if (imc < 25)
+            Console.WriteLine("Estado: Peso normal");
+        else if (imc < 30)
+            Console.WriteLine("Estado: Sobrepeso");
+        else
+            Console.WriteLine("Estado: Obesidad");
+    }
+}
