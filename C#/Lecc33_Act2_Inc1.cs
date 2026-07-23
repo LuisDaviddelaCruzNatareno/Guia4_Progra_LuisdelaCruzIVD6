3ca6fdd96864c008bf45ed4a09f0d54b169ac4d8
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-07-22 21:00:33 -0600

    Add files via upload

---
+++ C#/Lecc33_Act2_Inc1.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Luis de la Cruz\nGrado y sección: IVD\nClave: 6\n--- INVERTIR FRASE ---\n");
+
+        Console.Write("Ingrese una frase: ");
+        string frase = Console.ReadLine();
+
+        char[] arreglo = frase.ToCharArray();
+        Array.Reverse(arreglo);
+        string invertida = new string(arreglo);
+
+        Console.WriteLine($"\nFrase invertida: {invertida}");
+    }
+}
