f03f5bb963da5b6b55499afa76fa4a88485cec62
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-07-22 20:54:44 -0600

    Add files via upload

---
+++ C#/Lecc32_Act3_Inc1.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Luis de la Cruz\nGrado y sección: IVD\nClave: 6\n--- TIPO DE TRIÁNGULO ---\n");
+
+        Console.Write("Ingrese la medida del lado 1: ");
+        double l1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese la medida del lado 2: ");
+        double l2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese la medida del lado 3: ");
+        double l3 = Convert.ToDouble(Console.ReadLine());
+
+        if (l1 == l2 && l2 == l3)
+        {
+            Console.WriteLine("\nEl triángulo es Equilátero.");
+        }
+        else if (l1 == l2 || l1 == l3 || l2 == l3)
+        {
+            Console.WriteLine("\nEl triángulo es Isósceles.");
+        }
+        else
+        {
+            Console.WriteLine("\nEl triángulo es Escaleno.");
+        }
+    }
+}
